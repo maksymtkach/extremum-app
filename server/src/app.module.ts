26ee9f38a90caac400d185fe293fb3c17c9ba7e3
Maksym Tkach <maksymtkachcv@gmail.com>
2024-12-27 21:45:44 +0100

    feat: set up database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: '50024260',
+        database: 'myDatabase',
+        entities: [User],
+        synchronize: true,
+      }
+    ),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
